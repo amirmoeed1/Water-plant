a080a4df86d50f719bd693cbef8fb8c6094882b4
amirmoeed1 <112323482+amirmoeed1@users.noreply.github.com>
2024-09-24 00:35:55 +0500

    Update dashboard.js

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -25,10 +25,10 @@ const Dashboard = () => {
   };
 
   return (
-    <div className="container-fluid">
+    <div>
       {/* Navbar with Logout Button */}
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
+        <div className="container">
           <Link className="navbar-brand" to="/">Dashboard</Link>
           <button className="btn btn-danger ml-auto" onClick={handleLogout}>
             Logout
@@ -47,7 +47,7 @@ const Dashboard = () => {
               </div>
             </div>
           </div>
-
+         
           <div className="col-md-3">
             <div className="card text-center mb-4">
               <div className="card-body">
@@ -56,7 +56,6 @@ const Dashboard = () => {
               </div>
             </div>
           </div>
-
           <div className="col-md-3">
             <div className="card text-center mb-4">
               <div className="card-body">
@@ -65,12 +64,11 @@ const Dashboard = () => {
               </div>
             </div>
           </div>
-
           <div className="col-md-3">
             <div className="card text-center mb-4">
               <div className="card-body">
                 <h5 className="card-title">Customer Information</h5>
-                <Link to="/customerdata" className="btn btn-primary">Check Customer Detail</Link>
+                <Link to="/customerdata" className="btn btn-primary">Check Customer detail</Link>
               </div>
             </div>
           </div>
