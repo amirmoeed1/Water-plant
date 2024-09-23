5737ea83c76b423cfce27890c5fc395fa9a83fa1
amirmoeed1 <112323482+amirmoeed1@users.noreply.github.com>
2024-09-24 00:55:17 +0500

    Update dashboard.js

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -28,48 +28,49 @@ const Dashboard = () => {
     <div>
       {/* Navbar with Logout Button */}
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container">
-    <h3 className="text-primary fw-bold">Welcome to: Ijaz Ahmad Water-Plant</h3>
-    <button className="btn btn-danger ms-5" onClick={handleLogout}>
-      Logout
-    </button>
-  </div>
-</nav>
-
+        <div className="container">
+          <h3 className="text-primary fw-bold">Welcome to: Ijaz Ahmad Water-Plant</h3>
+          <button className="btn btn-danger ms-5" onClick={handleLogout}>
+            Logout
+          </button>
+        </div>
+      </nav>
 
       <div className="container mt-5">
         <h2 className="mb-4">Dashboard</h2>
         <div className="row">
-          <div className="col-lg-3">
-            <div className="card text-center mb-4">
+          <div className="col-lg-3 col-md-6 mb-4">
+            <div className="card text-center">
               <div className="card-body">
-                <h5 className="card-title">New Customer&Town Add</h5>
+                <h5 className="card-title">New Customer & Town Add</h5>
                 <Link to="/towns-customers" className="btn btn-primary">Go to Towns & Customers</Link>
               </div>
             </div>
           </div>
-         
-          <div className="col-lg-3">
-            <div className="card text-center mb-4">
+          
+          <div className="col-lg-3 col-md-6 mb-4">
+            <div className="card text-center">
               <div className="card-body">
                 <h5 className="card-title">Payment</h5>
                 <Link to="/payment" className="btn btn-primary">Check Customer Payment</Link>
               </div>
             </div>
           </div>
-          <div className="col-lg-3">
-            <div className="card text-center mb-4">
+          
+          <div className="col-lg-3 col-md-6 mb-4">
+            <div className="card text-center">
               <div className="card-body">
                 <h5 className="card-title">Monthly Report</h5>
                 <Link to="/report" className="btn btn-primary">Go to Monthly Report</Link>
               </div>
             </div>
           </div>
-          <div className="col-lg-3">
-            <div className="card text-center mb-4">
+          
+          <div className="col-lg-3 col-md-6 mb-4">
+            <div className="card text-center">
               <div className="card-body">
                 <h5 className="card-title">Customer Information</h5>
-                <Link to="/customerdata" className="btn btn-primary">Check Customer detail</Link>
+                <Link to="/customerdata" className="btn btn-primary">Check Customer Detail</Link>
               </div>
             </div>
           </div>
