261f089028e5a8ec02146160ba4f2e13e5ca3302
amirmoeed1 <112323482+amirmoeed1@users.noreply.github.com>
2024-09-24 00:32:54 +0500

    Update dashboard.js

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -28,7 +28,7 @@ const Dashboard = () => {
     <div>
       {/* Navbar with Logout Button */}
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container">
+        <div className="container-fluid">
           <Link className="navbar-brand" to="/">Dashboard</Link>
           <button className="btn btn-danger ml-auto" onClick={handleLogout}>
             Logout
