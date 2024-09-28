66fe9ca72a4fdfd1c70f813297bca64c6067df71
amirmoeed1 <112323482+amirmoeed1@users.noreply.github.com>
2024-09-29 00:54:53 +0500

    Update payment.js

--- Frontend/src/components/payment.js
+++ Frontend/src/components/payment.js
@@ -76,7 +76,7 @@ const Payment = () => {
 
   const fetchCustomers = async (townId) => {
     try {
-      const response = await axios.get('https://water-plant-backend.onrender.com/customer?townId=${townId}');
+      const response = await axios.get('https://water-plant-backend.onrender.com/customer?townId');
       setCustomers(response.data);
     } catch (error) {
       console.error('Error fetching customers:', error);
